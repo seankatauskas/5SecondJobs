83afba3ca098e3553a3e25a574f778315ba24f51
Sean Katauskas <seankatauskas@outlook.com>
2025-04-06 23:56:40 -0500

    removed working input

--- app/ui/ApplicationsDisplay.jsx
+++ app/ui/ApplicationsDisplay.jsx
@@ -65,9 +65,6 @@ export default function ApplicationsDisplay({ pageType, filters }) {
     <p>Error: {error.message}</p>
   ) : (
     <>
-    <div>
-    <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-</div>
       {data.pages.map((group, i) => (
         <React.Fragment key={i}>
           <div className='flex flex-col'>
